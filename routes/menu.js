0311432df3cc7799a6376c1244fbbd251f2ce664
ryan <sally41176789@yahoo.com.tw>
2020-07-11 17:14:08 +0800

    完成產品列表

--- routes/menu.js
+++ routes/menu.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const admin = require("firebase-admin");
+const serviceAccount = require("../project-f9b96-firebase-adminsdk-ybyij-e2736f502a.json");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('menu');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://project-f9b96.firebaseio.com"
 });
+
+const fireData = admin.database();
+
+function renderList(doc) {
+  let str = '';
+
+};
+
+router.get('/', function(req, res, next) {
+  fireData.ref('products').once('value', (snapshot) => {
+    const data = snapshot.val();
+    console.log(data[0].chTitle)
+    res.render('menu', { 'products': data });
+  })
+});
+
+
 
 module.exports = router;
